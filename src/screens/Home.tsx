58979ac904ff04e3f461dd096d706960aa0cf04c
fobdev <silvapedro55@gmail.com>
2021-11-13 13:31:24 -0300

    first implementation of screens

---
+++ src/screens/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+import { StandingsNavProp } from "../interfaces";
+
+export const Home = ({ navigation, route }: StandingsNavProp<"Home">) => {
+    return <View></View>;
+};
+export default Home;
---
+++ src/screens/StandingsTable.tsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+import { StandingsNavProp } from "../interfaces/stacks/StandingsStack";
+
+export const StandingsTable = ({ navigation, route }: StandingsNavProp<"Standings">) => {
+    return <View></View>;
+};
+export default StandingsTable;
