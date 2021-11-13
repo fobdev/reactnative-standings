98f2429bede0e1a0d7b9c5f3576a407219a3a349
fobdev <silvapedro55@gmail.com>
2021-11-13 17:18:36 -0300

    removed unecessary else statement

--- src/screens/StandingsTable.tsx
+++ src/screens/StandingsTable.tsx
@@ -28,94 +28,94 @@ export const StandingsTable = ({ navigation, route }: StandingsNavProp<"Standing
                 <ActivityIndicator color="#567d46" size={"large"} />
             </Center>
         );
-    else
-        return (
-            <DataTable>
-                <ScrollView overScrollMode="never">
-                    <DataTable.Header>
-                        <DataTable.Title style={{ flex: 5 }}>Time</DataTable.Title>
-                        {["P", "J", "V", "SG", "GP", "GC"].map((element, index) => {
-                            return (
-                                <DataTable.Title numeric key={index}>
-                                    {element}
-                                </DataTable.Title>
-                            );
-                        })}
-                    </DataTable.Header>
-                    {standingTasks?.data!.standings.map((element, index) => {
-                        let bgcolorChange = index % 2 === 0 ? "#efe" : "#fff";
 
-                        const winCount = findCount(element, "wins");
-                        const pointsCount = findCount(element, "points");
-                        const pointsForCount = findCount(element, "pointsFor");
-                        const gamesPlayedCount = findCount(element, "gamesPlayed");
-                        const pointsAgainstCount = findCount(element, "pointsAgainst");
-                        const pointDifferentialCount = findCount(element, "pointDifferential");
-
+    return (
+        <DataTable>
+            <ScrollView overScrollMode="never">
+                <DataTable.Header>
+                    <DataTable.Title style={{ flex: 5 }}>Time</DataTable.Title>
+                    {["P", "J", "V", "SG", "GP", "GC"].map((element, index) => {
                         return (
-                            <TouchableOpacity key={index}>
-                                <View>
-                                    <DataTable.Row
-                                        style={{
-                                            alignContent: "center",
-                                            backgroundColor: bgcolorChange,
-                                        }}
-                                    >
-                                        <View
-                                            style={{
-                                                flex: 5,
-                                                flexDirection: "row",
-                                                alignItems: "center",
-                                            }}
-                                        >
-                                            <View>
-                                                <Image
-                                                    source={
-                                                        element.team!.logos
-                                                            ? {
-                                                                  uri: element.team.logos[0].href,
-                                                                  width: 30,
-                                                                  height: 30,
-                                                              }
-                                                            : require("../../assets/noimage.png")
-                                                    }
-                                                    style={{
-                                                        width: 30,
-                                                        height: 30,
-                                                    }}
-                                                />
-                                            </View>
-                                            <View style={{ marginLeft: 5 }}>
-                                                <Text style={{ fontFamily: "Roboto_300Light" }}>
-                                                    {element.team.displayName.length < 20
-                                                        ? element.team.displayName
-                                                        : element.team.shortDisplayName}
-                                                </Text>
-                                            </View>
-                                        </View>
-                                        {[
-                                            winCount?.displayValue,
-                                            gamesPlayedCount?.displayValue,
-                                            pointsCount?.displayValue,
-                                            pointDifferentialCount?.displayValue,
-                                            pointsForCount?.displayValue,
-                                            pointsAgainstCount?.displayValue,
-                                        ].map((element, index) => {
-                                            return (
-                                                <DataTable.Cell numeric key={index}>
-                                                    <Text style={{ fontFamily: "Roboto_100Thin" }}>
-                                                        {element}
-                                                    </Text>
-                                                </DataTable.Cell>
-                                            );
-                                        })}
-                                    </DataTable.Row>
-                                </View>
-                            </TouchableOpacity>
+                            <DataTable.Title numeric key={index}>
+                                {element}
+                            </DataTable.Title>
                         );
                     })}
-                </ScrollView>
-            </DataTable>
-        );
+                </DataTable.Header>
+                {standingTasks?.data!.standings.map((element, index) => {
+                    let bgcolorChange = index % 2 === 0 ? "#efe" : "#fff";
+
+                    const winCount = findCount(element, "wins");
+                    const pointsCount = findCount(element, "points");
+                    const pointsForCount = findCount(element, "pointsFor");
+                    const gamesPlayedCount = findCount(element, "gamesPlayed");
+                    const pointsAgainstCount = findCount(element, "pointsAgainst");
+                    const pointDifferentialCount = findCount(element, "pointDifferential");
+
+                    return (
+                        <TouchableOpacity key={index}>
+                            <View>
+                                <DataTable.Row
+                                    style={{
+                                        alignContent: "center",
+                                        backgroundColor: bgcolorChange,
+                                    }}
+                                >
+                                    <View
+                                        style={{
+                                            flex: 5,
+                                            flexDirection: "row",
+                                            alignItems: "center",
+                                        }}
+                                    >
+                                        <View>
+                                            <Image
+                                                source={
+                                                    element.team!.logos
+                                                        ? {
+                                                              uri: element.team.logos[0].href,
+                                                              width: 30,
+                                                              height: 30,
+                                                          }
+                                                        : require("../../assets/noimage.png")
+                                                }
+                                                style={{
+                                                    width: 30,
+                                                    height: 30,
+                                                }}
+                                            />
+                                        </View>
+                                        <View style={{ marginLeft: 5 }}>
+                                            <Text style={{ fontFamily: "Roboto_300Light" }}>
+                                                {element.team.displayName.length < 20
+                                                    ? element.team.displayName
+                                                    : element.team.shortDisplayName}
+                                            </Text>
+                                        </View>
+                                    </View>
+                                    {[
+                                        winCount?.displayValue,
+                                        gamesPlayedCount?.displayValue,
+                                        pointsCount?.displayValue,
+                                        pointDifferentialCount?.displayValue,
+                                        pointsForCount?.displayValue,
+                                        pointsAgainstCount?.displayValue,
+                                    ].map((element, index) => {
+                                        return (
+                                            <DataTable.Cell numeric key={index}>
+                                                <Text style={{ fontFamily: "Roboto_100Thin" }}>
+                                                    {element}
+                                                </Text>
+                                            </DataTable.Cell>
+                                        );
+                                    })}
+                                </DataTable.Row>
+                            </View>
+                        </TouchableOpacity>
+                    );
+                })}
+            </ScrollView>
+        </DataTable>
+    );
 };
 export default StandingsTable;
